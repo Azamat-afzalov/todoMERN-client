e4ca57e49a3490d3e75bff83802b369303a681ef
Azamat-afzalov <52107423+Azamat-afzalov@users.noreply.github.com>
2020-05-19 18:24:34 +0500

    added backgroun and form

--- src/components/TodoAddForm.js
+++ src/components/TodoAddForm.js
@@ -5,7 +5,15 @@ import './Form.css';
 const TodoAddForm = () => {
     return (
         <form className='TodoAddForm-form'>
-            <input type="text" className="TodoAddForm-input" name="todo"/>
+            <div className="TodoAddForm-input-container">
+                <input
+                    type="text"
+                    className="TodoAddForm-input"
+                    name="todo"
+                    placeholder="What is your plans ?"
+                />
+                <span className='TodoAddForm-clearInput'>&times;</span>
+            </div>
             <button type="submit" className="TodoAddForm-button" >Add</button>
         </form>
     )
